88efb5fe2e40acb20ab2d35937e628631e896355
Zachary Lloyd <zaclloyd@uat.edu>
2021-02-08 20:56:28 -0500

    Started working with spawn algorithm
    Need some rework to get a better calculation of where to spawn towards. Then find the nearest spawn point to that point and instantiate the player there.

--- UnitTestISS/Assets/Scripts/SpawnManager.cs
+++ UnitTestISS/Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,31 @@ using UnityEngine.UI;
 
 public class SpawnManager : MonoBehaviour
 {
-    [Header("Lists needed to track data")]
+    //Ideal position for player to spawn which will be used to find the neariest spawn point to this location
+    [SerializeField] Transform idealSpawnLocation;
+    //Actual spawn point the player will be instantiated on
+    [SerializeField] Transform pointToSpawnOn;
+
+    //May get removed
+    Vector3 idealLoction;
+    Vector3 sumOf2Calcs;
+    float totalImportance;
+
+
+    [SerializeField] Vector3 pref1Calc;
+    [SerializeField] Vector3 pref2Calc;
+    [SerializeField] Vector3 pref3Calc;
+
+    //May get removed
+    public GameObject objectToSpawn;
+    public GameObject hishestSpawn;
+
+
     //Singleton for reference to SpawnManager
     public static SpawnManager Instance;
     //Position that will be used when spawning
-    [HideInInspector]public Transform playerLastPosition;
+    public Transform playerLastPosition;
+    [Header("Lists needed to track data")]
     //All players on level
     public List<GameObject> players = new List<GameObject>();
     //All enemies on level
@@ -20,7 +40,8 @@ public class SpawnManager : MonoBehaviour
     public List<GameObject> friendlyBase = new List<GameObject>();
     //All enemy bases on level
     public List<GameObject> enemyBase = new List<GameObject>();
-    [Header("UI pieces needed")]//Drag these into place accordingly
+
+    [Header("UI pieces needed")]
     //Slider that will represent how far or close the player wants to spawn from enemies
     public Slider pref1;
     //Slider that will represent how far or close the player wants to spawn from a friendly base or enemy base
@@ -33,6 +54,7 @@ public class SpawnManager : MonoBehaviour
     public Slider importancePref2;
     //Importance factor for pref3
     public Slider importancePref3;
+
     [Header("Variables to hold value for sliders")]
     //Value for pref1 to be saved
     public float pref1Value;
@@ -46,6 +68,7 @@ public class SpawnManager : MonoBehaviour
     public float importancePref2Value;
     //Importance value for pref3 to be saved
     public float importancePref3Value;
+
     //Awake is called before the anything else
     void Awake()
     {
@@ -88,6 +111,18 @@ public class SpawnManager : MonoBehaviour
             enemyBase.Add(eBase);
         }
         #endregion
+       
+        //Assigning sliders
+        #region
+        pref1 = GameObject.FindGameObjectWithTag("pref1").GetComponent<Slider>();
+        pref2 = GameObject.FindGameObjectWithTag("pref2").GetComponent<Slider>();
+        pref3 = GameObject.FindGameObjectWithTag("pref3").GetComponent<Slider>();
+        importancePref1 = GameObject.FindGameObjectWithTag("iPref1").GetComponent<Slider>();
+        importancePref2 = GameObject.FindGameObjectWithTag("iPref2").GetComponent<Slider>();
+        importancePref3 = GameObject.FindGameObjectWithTag("iPref3").GetComponent<Slider>();
+        #endregion
+
+
         LoadValues();
     }
     //Save function for values to be stored
@@ -108,6 +143,8 @@ public class SpawnManager : MonoBehaviour
         PlayerPrefs.SetFloat("importancePref2", importancePref2Value);
         PlayerPrefs.SetFloat("importancePref3", importancePref3Value);
         #endregion
+        CalculateIdealSpawn();
+
     }
     //Load function for values to be assigned based off last save
     public void LoadValues()
@@ -134,5 +171,45 @@ public class SpawnManager : MonoBehaviour
         importancePref2.value = importancePref2Value;
         importancePref3.value = importancePref3Value;
         #endregion
+    }
+    //Used to calculate the ideal spawn location
+    void CalculateIdealSpawn()
+    {
+        //enemy calc
+        pref1Calc = Vector3.Lerp(playerLastPosition.position, enemies[0].gameObject.transform.position, pref1Value );
+        //base calc
+        pref2Calc = Vector3.Lerp(friendlyBase[0].gameObject.transform.position, enemyBase[0].gameObject.transform.position, pref2Value );
+        //elavation calc
+        pref3Calc = Vector3.Lerp(playerLastPosition.position, hishestSpawn.transform.position, pref3Value ) ;
+
+        //This may get removed
+        totalImportance = importancePref1Value + importancePref2Value + importancePref3Value;
+        
+        //This may get removed
+        sumOf2Calcs = Vector3.Lerp(pref1Calc, pref2Calc, totalImportance);
+        idealLoction = Vector3.Lerp(sumOf2Calcs, pref3Calc, totalImportance);
+
+        //Instantiate(objectToSpawn, pref1Calc, Quaternion.identity);
+        //Instantiate(objectToSpawn, pref2Calc, Quaternion.identity);
+        //Instantiate(objectToSpawn, pref3Calc, Quaternion.identity);
+        //Instantiate(objectToSpawn, idealLoction, Quaternion.identity);
+
+        Debug.Log($"{pref1Calc},\n{pref2Calc},\n{pref3Calc}\n{sumOf2Calcs}\n{idealLoction}");
+
+        //Debug.DrawLine(playerLastPosition.position, enemies[0].gameObject.transform.position, Color.green, 10f);
+        //Debug.DrawLine(friendlyBase[0].gameObject.transform.position, enemyBase[0].gameObject.transform.position, Color.red, 10f);
+        //Debug.DrawLine(playerLastPosition.position, hishestSpawn.transform.position, Color.blue, 10f);
+        //Debug.DrawLine(pref1Calc, pref2Calc, Color.yellow, 10f);
+        //Debug.DrawLine(sumOf2Calcs, pref3Calc, Color.gray, 10f);
+    }
+    //Used to find the neareest spawn point to the ideal spawn found
+    void CalculateNearestSpawn()
+    {
+
+    }
+    //Instantiate player on the nearest spawn point to the ideal spawn found
+    void SpawnPlayer()
+    {
+
     }
 }
